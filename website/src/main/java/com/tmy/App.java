935912fcf44a1bb2edd5dac9f10f02f44065606a
xr <xierui.serc@gmail.com>
2015-04-25 13:20:33 +0800

    add home

--- website/src/main/java/com/tmy/App.java
+++ website/src/main/java/com/tmy/App.java
@@ -1,13 +1,24 @@
 package com.tmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableAutoConfiguration
+@ComponentScan
+@EnableWebMvc
+public class App extends SpringBootServletInitializer  {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
     }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
 }
