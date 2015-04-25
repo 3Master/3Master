49dd312fe63993504da8dbfb317f47b283e34820
harttle <yangjvn@126.com>
2015-04-25 20:01:17 +0800

    message page

--- website/src/main/resources/static/js/message.js
+++ website/src/main/resources/static/js/message.js
@@ -2,13 +2,6 @@
  * Created by harttle on 15/4/25.
  */
 
-function onRequest(userId){
 
-    $.post( '/request/' + userId)
-        .done(function(){
-
-        })
-        .fail(function(){
-            console.error('request failed:', userId)
-        })
-}
+$(function(){
+});
