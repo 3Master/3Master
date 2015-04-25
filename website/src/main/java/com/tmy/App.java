e87c65845c048a5310e62aad3addc33cf50a2118
xr <xierui.serc@gmail.com>
2015-04-25 15:32:42 +0800

    support jsp[

--- website/src/main/java/com/tmy/App.java
+++ website/src/main/java/com/tmy/App.java
@@ -5,18 +5,18 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @EnableAutoConfiguration
 @ComponentScan
-@EnableWebMvc
 public class App extends SpringBootServletInitializer  {
     
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(App.class);
     }
-
+    
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
--- website/src/main/java/com/tmy/controller/HomeController.java
+++ website/src/main/java/com/tmy/controller/HomeController.java
@@ -3,12 +3,19 @@ package com.tmy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     
     @RequestMapping(value = "", method=RequestMethod.GET)
     public String home(){
+        return "index";
+    }
+    
+    @RequestMapping(value = "/test", method=RequestMethod.GET)
+    @ResponseBody
+    public String test(){
         return "index";
     }
 
