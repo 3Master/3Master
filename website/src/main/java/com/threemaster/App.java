01512595adef3b4c30d55a080cb5f893c834d7df
xr <xierui.serc@gmail.com>
2015-04-25 20:45:21 +0800

    save

--- website/src/main/java/com/threemaster/App.java
+++ website/src/main/java/com/threemaster/App.java
@@ -1,15 +1,20 @@
 package com.threemaster;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+import com.threemaster.filter.MethodFiletr;
 
 @Configuration
 @EnableAutoConfiguration
@@ -27,9 +32,11 @@ public class App extends SpringBootServletInitializer  {
     }
     
     @Bean
-    @ConditionalOnMissingBean(HiddenHttpMethodFilter.class)
-    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
-        return new HiddenHttpMethodFilter();
+    public FilterRegistrationBean myFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new MethodFiletr());
+        registration.setDispatcherTypes(EnumSet.allOf(DispatcherType.class));
+        return registration;
     }
-
+    
 }
